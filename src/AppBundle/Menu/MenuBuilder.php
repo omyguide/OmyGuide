b2af5197d5daad05df1c89c8af6287fa351be59a
root <bertrand.galimard@gmail.com>
2018-05-02 04:20:33 +0200

    integration bootstrap

--- src/AppBundle/Menu/MenuBuilder.php
+++ src/AppBundle/Menu/MenuBuilder.php
@@ -19,55 +19,39 @@ class MenuBuilder
 
     public function createMainMenu(array $options)
     {
-        $menu = $this->factory->createItem('root');
+        $menu = $this->factory->createItem('root',array('childrenAttributes' => array('class' => 'navbar-nav ml-auto')));
 
         $menu->addChild('layout.register',
-          array('route' => 'fos_user_registration_register',
-            'attributes' => array('class' => 'header-nav-li'),
-            'linkAttributes' => array('class' => 'header-nav-text')
-          )
+          array('route' => 'fos_user_registration_register')
         )->setExtra('translation_domain', 'FOSUserBundle');
 
         if ($this->authorization_checker->isGranted('IS_AUTHENTICATED_REMEMBERED')) {
             $profile_picture=$this->token_storage->getToken()->getUser()->getProfilePicture();
-            $label=$profile_picture?'<img class="img-round mod-pic" src="'.$profile_picture.'"/>':$this->token_storage->getToken()->getUser()->getFirstName();
+            $label=$profile_picture?'<img class="rounded-circle fb-pic" src="'.$profile_picture.'"/>':$this->token_storage->getToken()->getUser()->getFirstName();
             $profile=$menu->addChild($label,
-              array('attributes' => array('class' => 'header-nav-li dropdown'),
-                'uri' => '#',
-                'linkAttributes' => array('class' => $profile_picture?'header-nav-pic':'header-nav-text')
+              array('attributes' => array('dropdown' => true),
+                'uri' => 'javascript:void(0)',
+                'linkAttributes' => array('class' => $profile_picture?'header-nav-pic':null)
               )
             )->setExtra('safe_label', true );
-
-            $profile->setChildrenAttribute('class','dropdown-menu');
-
-            // Arrow
-            $profile->addChild('',
-              array('attributes' => array('class' => 'dropdown-arrow-top')
-              )
-            );
 
             // Edit profile
             $profile->addChild('profile.edit',
               array('route' => 'fos_user_profile_edit',
-                'attributes' => array('class' => 'header-nav-sub-li'),
-                'linkAttributes' => array('class' => 'header-nav-sub-text')
+                'attributes' => array('divider_append' => true)
               )
             );
 
             // Log out
             $profile->addChild('layout.logout',
               array('route' => 'fos_user_security_logout',
-                'attributes' => array('class' => 'header-nav-sub-li'),
-                'linkAttributes' => array('class' => 'header-nav-sub-text')
+                'attributes' => array('icon' => 'fa fa-sign-out')
               )
             )->setExtra('translation_domain', 'FOSUserBundle');
         }
         else {
             $menu->addChild('layout.login',
-              array('route' => 'fos_user_security_login',
-                'attributes' => array('class' => 'header-nav-li'),
-                'linkAttributes' => array('class' => 'header-nav-text')
-              )
+              array('route' => 'fos_user_security_login')
             )->setExtra('translation_domain', 'FOSUserBundle');
         }
 
